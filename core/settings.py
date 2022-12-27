7be9e2dc400f3a716925b805ba0abe13496db46b
mpl73 <108297593+mpl73@users.noreply.github.com>
2022-12-27 14:46:36 -0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -27,8 +27,8 @@ DEBUG = env('DEBUG')
 ASSETS_ROOT = os.getenv('ASSETS_ROOT', '/static/assets') 
 
 # load production server from .env
-ALLOWED_HOSTS        = ['localhost', 'localhost:85', '127.0.0.1', '192.168.1.102',env('SERVER', default='127.0.0.1')]
-CSRF_TRUSTED_ORIGINS = ['http://localhost:85', 'http://127.0.0.1', 'https://192.168.1.102','https://' + env('SERVER', default='127.0.0.1') ]
+ALLOWED_HOSTS        = ['localhost', 'localhost:85', '127.0.0.1', '192.168.0.23',env('SERVER', default='127.0.0.1')]
+CSRF_TRUSTED_ORIGINS = ['http://localhost:85', 'http://127.0.0.1', 'https://192.168.0.23','https://' + env('SERVER', default='127.0.0.1') ]
 
 # Application definition
 
